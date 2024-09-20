1d4d8b713ad272437ab0592ee59f7a513be21514
Figby222 <figby222@outlook.com>
2024-09-20 02:53:45 -0700

    Add signUpPost() controller

--- controllers/indexController.mjs
+++ controllers/indexController.mjs
@@ -1,3 +1,5 @@
+import pool from "../db/pool.mjs";
+
 function indexRouteGet(req, res) {
     res.render("index", { title: "User Login Page" });
 }
@@ -6,4 +8,17 @@ function signUpFormGet(req, res) {
     res.render("/sign-up-form")
 }
 
-export { indexRouteGet, signUpFormGet };
+async function signUpPost(req, res) {
+    try {
+        await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+            req.body.username,
+            req.body.password
+        ])
+
+        res.redirect("/");
+    } catch (err) {
+        return next(err);
+    }
+}
+
+export { indexRouteGet, signUpFormGet, signUpPost };
