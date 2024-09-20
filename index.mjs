ee7918b1119a0abe5429dcab4d14dace7b05d89c
Figby222 <figby222@outlook.com>
2024-09-20 03:29:09 -0700

    Add localStrategy authentication

--- index.mjs
+++ index.mjs
@@ -6,6 +6,7 @@ import path from "node:path";
 import session from "express-session";
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
+import pool from "./db/pool.mjs";
 
 const __dirname = import.meta.dirname;
 
@@ -15,6 +16,25 @@ app.set("view engine", "ejs");
 app.use(session({ secret: "cats", resave: false, saveUniitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
+
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try{
+            const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+            const user = rows[0];
+
+            if (!user) {
+                return done(null, false, { message: "Incorrect username" });
+            }
+            if (user.password !== password) {
+                return done(null, false, { message: "Incorrect password" });
+            }
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    })
+)
 
 app.use("/", indexRouter);
 
