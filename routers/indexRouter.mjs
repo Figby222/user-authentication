379df159b363c63be3be93d575dc8496f99c8130
Figby222 <figby222@outlook.com>
2024-09-20 02:45:25 -0700

    Add /sign-up GET route

--- routers/indexRouter.mjs
+++ routers/indexRouter.mjs
@@ -3,6 +3,8 @@ import * as indexController from "../controllers/indexController.mjs";
 
 const indexRouter = Router();
 
+indexRouter.get("/sign-up", indexController.signUpFormGet);
+
 indexRouter.get("/", indexController.indexRouteGet);
 
 export default indexRouter;
