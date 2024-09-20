6c17e9c2984108128c081161ed834ce5058d33d9
Figby222 <figby222@outlook.com>
2024-09-20 02:39:38 -0700

    Setup session with passport

--- index.mjs
+++ index.mjs
@@ -3,12 +3,17 @@ const app = express();
 import indexRouter from "./routers/indexRouter.mjs";
 import "dotenv/config";
 import path from "node:path";
+import session from "express-session";
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
 
 const __dirname = import.meta.dirname;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(session({ secret: "cats", resave: false, saveUniitialized: false }));
+app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
